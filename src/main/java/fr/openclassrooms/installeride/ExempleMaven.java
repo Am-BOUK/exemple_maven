814d44e6b1b9a02a158049462a9f0bf21019e5fb
Amal <ab.amal.boukili@gmail.com>
2021-06-15 17:31:44 +0200

    simple using git flow

---
+++ src/main/java/fr/openclassrooms/installeride/ExempleMaven.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.installeride;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+public class ExempleMaven {
+
+	public static void main(String[] args) {
+		ImagePlus imp = IJ.openImage();
+		imp.show();
+	}
+
+}
